475633abc80178b21199a919dcb1ca05a472f669
Boronovvva <saraboronova003@gmail.com>
2024-04-28 13:20:25 +0600

    as

---
+++ apps/todolist/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from apps.todolist.models import ToDo
+
+class ToDoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = ('id','title','description','image','is_completed','created_at')
